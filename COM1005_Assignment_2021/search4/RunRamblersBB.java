3a8fec37686240c867766d5fad13d6c5b7cc032c
allinawaz <allinawaz@hotmail.com>
2021-05-19 14:39:34 +0100

    Add Run Rambler Class

---
+++ COM1005_Assignment_2021/search4/RunRamblersBB.java
@@ -0,0 +1,14 @@
+
+public class RunRamblersBB {
+
+	public static void main(String[] args) {
+		
+		TerrainMap terrainMap = new TerrainMap("tmc.pgm");
+		Coords  target = new Coords(20,20);
+
+		
+	
+		
+	}
+
+}
