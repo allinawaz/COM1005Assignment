581465e99ca8e8742e660977a02a0b5220b20753
allinawaz <allinawaz@hotmail.com>
2021-05-28 11:18:09 +0100

    Main Class Branch And Bound

--- COM1005_Assignment_2021/search4/RunRamblersBB.java
+++ COM1005_Assignment_2021/search4/RunRamblersBB.java
@@ -1,17 +1,17 @@
 
+
 public class RunRamblersBB {
 
 	public static void main(String[] args) {
 		
-		TerrainMap terrainMap = new TerrainMap("search3/tmc.pgm");
-		Coords  target = new Coords(5,6);
+		TerrainMap terrainMap = new TerrainMap("search4/tmc.pgm");
+		Coords  target = new Coords(10,10);
 
 		RamblersSearch searcher = new RamblersSearch(terrainMap, target);
 		SearchState initState = (SearchState) new RamblersState(new Coords(0,1),3);
 		String resultBranchAndBound = searcher.runSearch(initState,"branchAndBound");
 		System.out.println("Total Cost is: "+resultBranchAndBound);
-	
-		
+
 	}
 
 }
