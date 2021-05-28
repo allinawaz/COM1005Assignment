662e9253e7aa98fac5d6e42481629965425f1e20
allinawaz <allinawaz@hotmail.com>
2021-05-28 11:16:49 +0100

    Add AStar class heuristics

--- COM1005_Assignment_2021/search4/RamblersSearch.java
+++ COM1005_Assignment_2021/search4/RamblersSearch.java
@@ -1,26 +1,31 @@
 
 public class RamblersSearch extends Search {
-	private TerrainMap terrainMap;
-	private Coords target;
-	
-	
+	private TerrainMap terrainMap;	// map to explore
+	private Coords target; 			// goal coordinates
+	private String heuristicType = "manhattan";
+	//Ramblers Search Constructor
 	public RamblersSearch(TerrainMap terrainMap, Coords target) {
 		this.terrainMap = terrainMap;
 		this.target = target;
 	}
+	
+	//accessor methods of RamblerSearch
 	public TerrainMap getTerrainMap() {
 		return terrainMap;
 	}
-	public void setTerrainMap(TerrainMap terrainMap) {
-		this.terrainMap = terrainMap;
-	}
+	
 	public Coords getTarget() {
 		return target;
 	}
-	public void setTarget(Coords target) {
-		this.target = target;
+	
+
+	public String getHeuristicType() {
+		return heuristicType;
 	}
 	
+	public void setHeuristicType(String heuristicType) {
+		this.heuristicType = heuristicType;
+	}
 	
 
 }
